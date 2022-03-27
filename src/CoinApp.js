ea4c9bc6f24d4e9e33a7ecb571a7ca153819499b
sungwon <sungwon_cho@fursys.com>
2022-03-27 22:59:28 +0900

    챌린지 :  선택한 코인 가격을 비트코인과 비교해보기 - Q. 비트코인 가격을 state로 설정하지 않고, const price 와 같이 변수로   지정하면 api가 호출 시점과 맞지 않아 오류가 발생하는듯..? -> state로   지정해주면 오류는 나지 않음. - loading === true일때만 화면을 그리고 싶은데, 모든 요소들을   각각{loading ? : }에 넣어야하나? 한 번에 처리할 수 있는 방법이   없을까? -> 화면을 컴포넌트화 시켜서 불러오자

--- src/CoinApp.js
+++ src/CoinApp.js
@@ -6,23 +6,49 @@ function CoinApp() {
     // 1. coin들의 정보를 담을 리스트(coins), coins를 업데이트 해주기 위한 setCoins
     // 초기 값은 빈 array임
     const [coins, setCoins] = useState([]);
-
     // 브라우저가 실행될 때 최초 1번 코인 정보를 받아올 것. 최초 1번만 실행될 것이기 때문에, 표적 함수에 아무것도 넣지 않음
     useEffect(() => {
         fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
-            .then((res) => res.json())
-            .then((json) => {
-                setTimeout(function(){
-                    setCoins(json);
-                    setLoading(true);
-                }, 1000);
-                }
-            );
-    }, []);
+        .then((res) => res.json())
+        .then((json) => {
+            setCoins(json);
+            setLoading(true);
+        }
+        )
+    }, [])
+
+    // 챌린지 : 선택한 코인을 비트코인 단위로 환산해보자
+    // 선택된 코인의 가격
+    const [slctPrice, setSlctPrice] = useState(0);
+    // 선택된 코인 이름
+    const [slctCoin, setSlctCoin] = useState('');
+    // 비트코인 가격
+    const [bitPrice, setBitPrice] = useState('');
+    
+    function sltCoin(){
+        // 선택한 코인 idx 뽑음
+        var coinIdxNum = document.getElementById('coinSlctBx').value.split('.', 1).toString();
+        var coinIdx = coinIdxNum - 1;
+        var slctPrice = coins[coinIdx].quotes.USD.price;
+        var slctCoin = coins[coinIdx].name;
+        setSlctPrice(slctPrice);
+        setSlctCoin(slctCoin);
+        setBitPrice(coins[0].quotes.USD.price);
+    }
 
     return(
         <div>
-            {loading ? <h1>Coins Ranking List</h1> : <h1>Loading...</h1>}
+            {loading ? <h1>비트코인과 비교해보기</h1> : ''}
+            {loading ?
+            <select onChange={sltCoin} id='coinSlctBx'>
+            {coins.map((coin, index) => 
+                <option key={coin.id}>{index+1}. {coin.name}</option>
+                )}
+            </select>    
+            : ''}
+            {loading ? <span>1 BitCoin = {Math.round(bitPrice/slctPrice)} {slctCoin}</span> : ''}
+            
+            {loading ? <h1>전체 코인 리스트</h1> : <h1>Loading...</h1>}
             <ul>
                 {coins.map((coin, index) => 
                     <li key={coin.id}>{index+1}. {coin.name} - {Math.round(coin.quotes.USD.price) / 100}</li>
